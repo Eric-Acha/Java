b7292398732485a758291b2622a7b4f58c647a12
Eric-Acha <eric.techcore1@gmail.com>
2025-04-14 06:20:26 -0400

    Using while loop

---
+++ while_loop.java
@@ -0,0 +1,11 @@
+public class while_loop {
+    public static void main(String[] args) {
+        int num = 15;
+
+        while (num < 30) {
+            System.out.println("The value of number : " + num);
+            num++;
+            // System.out.println("\n");
+        }
+    }
+}
