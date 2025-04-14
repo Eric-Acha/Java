6d9e7606d9395fda27b320d0f4de61f6c1be1d57
Eric-Acha <eric.techcore1@gmail.com>
2025-04-14 06:51:13 -0400

    Using do while loops

---
+++ do_while_loop.java
@@ -0,0 +1,13 @@
+public class do_while_loop {
+
+    public static void main(String[] args) {
+        int x = 20;
+
+        do {
+            System.out.println("x is" + x);
+            x++;
+            System.out.println("\n");
+
+        } while (x < 30);
+    }
+}
