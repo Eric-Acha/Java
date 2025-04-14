576e30892e9caf0d903594d3d42b073905925449
Eric-Acha <eric.techcore1@gmail.com>
2025-04-14 11:45:07 -0400

    declaring, instantiating and initializing arrays

---
+++ Testarry.java
@@ -0,0 +1,14 @@
+public class Testarry {
+
+    public static void main(String[] args) {
+        int a[] = new int[6]; // declaration & instansiation of an arry in java
+        a[0] = 5; // initialization
+        a[1] = 10;
+        a[2] = 15;
+        a[3] = 20;
+        a[4] = 25;
+        a[5] = 30;
+        for (int i = 0; i < a.length; i++)
+            System.out.println(a[i]);
+    }
+}
