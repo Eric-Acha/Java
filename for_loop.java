6a25ee959e45a3fbd47b74173d2319d1265db959
Eric-Acha <eric.techcore1@gmail.com>
2025-04-14 06:34:12 -0400

    Using for loops

---
+++ for_loop.java
@@ -0,0 +1,11 @@
+public class for_loop {
+
+    public static void main(String[] args) {
+        for (int num = 12; num < 14; num = num + 1) {
+            System.out.println("Number is :" + num);
+            System.out.println("\n");
+
+        }
+
+    }
+}
