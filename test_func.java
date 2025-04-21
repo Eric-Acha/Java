4361686cab6fef8a828e264ae65a0865bd0502b7
Eric-Acha <eric.techcore1@gmail.com>
2025-04-21 11:06:19 -0400

    first java function

---
+++ test_func.java
@@ -0,0 +1,13 @@
+public class test_func {
+
+    public static void main(String[] args) {
+        int c = sum(4, 5);
+        System.out.println(c);
+
+    }
+
+    public static int sum(int a, int b) {
+        return a + b;
+
+    }
+}
